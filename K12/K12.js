2f535c0d6ebac9a5e5efd0d487412cba13f83fae
TechPro <phineasm4065@utahvirtual.org>
2021-10-20 11:25:54 -0600

    Update to 1.5.8

---
+++ K12/K12.js
@@ -0,0 +1,4 @@
+var icons = document.head.querySelectorAll("link[rel*='icon']");
+for(var i = 0; i < icons.length; i++){
+    icons[i].href = "https://techpro-services.github.io/Images/K12%20Enhancer.png";
+}
